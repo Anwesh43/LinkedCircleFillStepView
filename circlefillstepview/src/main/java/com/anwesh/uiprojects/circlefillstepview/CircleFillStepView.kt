ea66856fc0f44e49edeb0c8cbed8b85d7a2f44ad
Anwesh43 <anweshthecool0@gmail.com>
2018-10-14 09:58:31 +0530

    adding CFSNNode class

--- circlefillstepview/src/main/java/com/anwesh/uiprojects/circlefillstepview/CircleFillStepView.kt
+++ circlefillstepview/src/main/java/com/anwesh/uiprojects/circlefillstepview/CircleFillStepView.kt
@@ -101,4 +101,48 @@ class CircleFillStepView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CFSNode(var i : Int, val state : State = State()) {
+
+        private var next : CFSNode? = null
+        private var prev : CFSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CFSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCFSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CFSNode {
+            var curr : CFSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
