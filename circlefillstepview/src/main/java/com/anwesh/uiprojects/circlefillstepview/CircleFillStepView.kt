e7aa72d77cfdcba55ad4db308c4ffc872e5543fb
Anwesh43 <anweshthecool0@gmail.com>
2018-10-14 08:00:21 +0530

    importing libraries and defining nodes

--- circlefillstepview/src/main/java/com/anwesh/uiprojects/circlefillstepview/CircleFillStepView.kt
+++ circlefillstepview/src/main/java/com/anwesh/uiprojects/circlefillstepview/CircleFillStepView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.circlefillstepview
 /**
  * Created by anweshmishra on 14/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Path
+import android.content.Context
+
+val nodes : Int = 5
